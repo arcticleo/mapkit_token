8b1f6607437642d083ae6c10e3d05147bfd010f8
Michael Edlund <medlund@mac.com>
2019-08-13 16:01:07 -0700

    Require files

--- lib/mapkit_token.rb
+++ lib/mapkit_token.rb
@@ -1,5 +1,8 @@
+require "active_support/all"
 require "mapkit_token/version"
+require "mapkit_token/config/routes.rb"
+require "mapkit_token/app/models/mapkit.rb"
+require "mapkit_token/app/controllers/mapkit_token_controller.rb"
 
 module MapkitToken
-  # Your code goes here...
 end
